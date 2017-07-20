8e215f9896521bdc4bf17d91e6ae432173d62333
Mohammed Hijazi <mohammedhijazi199@gmail.com>
2017-07-20 10:06:00 +0300

    added contact page

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 import random
 app = Flask(__name__)
-
-
-
-
-
 
 
 
@@ -20,7 +15,24 @@ def about_me():
 	return render_template("aboutme.html")
 
 
+@app.route("/contact")
+def contact_us():
+	return render_template("contactus.html")
+
+
+
+
+@app.route("/form_response",methods=["POST"])
+def form_res():
+	user_firstname = request.form["firstname"]
+	user_lastname = request.form["lastname"]
+	user_message = request.form["message"]
+	user_gender = request.form["gender"]
+	return render_template ("formdata.html",userfirstname=user_firstname,userlastname=user_lastname,
+	usermessage=user_message,usergender=user_gender)
+
+
+
 
 if __name__ == "__main__":
 	app.run(port=5052)
-
