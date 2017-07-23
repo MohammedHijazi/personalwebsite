73d544f040f057c55842cbf63844e9239781f8c0
Mohammed Hijazi <mohammedhijazi199@gmail.com>
2017-07-23 15:37:01 +0300

    logo

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask,render_template, request
+import dataset
 import random
 app = Flask(__name__)
+
+
+# db=dataset.connect("postgres://yowahpfaxtsjoc:89e78de430594fa7bffec234ce5bc8ac9193cef0a864378fd900ddd573d65651@ec2-23-23-244-83.compute-1.amazonaws.com:5432/dbn2rcviq5s3i4")
+# table=db["hijazi_user"]
 
 
 
 @app.route("/")
 def home_page():
-	nam=["mohammed", "Bakiza" , "odai" , "khaled"]
-	return render_template("index.html",name=random.choice(nam))
+	return render_template("index.html") 
 
 
 @app.route("/about")
@@ -28,8 +32,19 @@ def form_res():
 	user_lastname = request.form["lastname"]
 	user_message = request.form["message"]
 	user_gender = request.form["gender"]
-	return render_template ("formdata.html",userfirstname=user_firstname,userlastname=user_lastname,
-	usermessage=user_message,usergender=user_gender)
+
+	# hijazi_user.insert(dict(name=user_firstname
+							# ,userlastname=user_lastname
+							# ,usermassege=user_message
+							# ,usergender=user_gender))
+	# print hijazi_user
+
+# @app.route("/displayinfo")
+# def display():
+# 	dis=hijazi_user.findone(name="user_firstname")
+# 	return render_template("dis.html")
+
+
 
 
 
